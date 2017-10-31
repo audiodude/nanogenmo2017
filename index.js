35eb774a3ebe1235cc0c6b3adcea5e351f3742f2
Travis Briggs <audiodude@gmail.com>
2017-10-31 16:13:44 -0700

    Initial commit, pretty close to tutorial

---
+++ index.js
@@ -0,0 +1,20 @@
+const Improv = require('improv');
+const yaml = require('js-yaml');
+const fs = require('fs-jetpack');
+
+// Load our data from a file
+const grammarData = yaml.load(fs.read('grammar.yaml'));
+
+// Create a generator object from this data
+const generator = new Improv(grammarData, {
+  filters: [
+    Improv.filters.mismatchFilter(),
+    Improv.filters.partialBonus(),
+    Improv.filters.fullBonus(),
+    Improv.filters.dryness()
+  ],
+  reincorporate: true
+});
+
+// Generate text and print it out
+console.log(generator.gen('root', {}));
